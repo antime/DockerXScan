5fd6119705fef6ad4b7ec8a2af7d02aee6aef610
mxi4oyu <798033502@qq.com>
2017-07-20 16:30:49 +0800

    fix namespace error

--- database/pgsql/namespace.go
+++ database/pgsql/namespace.go
@@ -34,26 +34,25 @@ func (pgSQL *pgSQL)InsertNamespace(namespace database.Namespace) (int ,error)  {
 
 func (pgSQL *pgSQL) ListNamespaces()  (namespaces []database.Namespace, err error) {
 	rows, err := pgSQL.Query(listNamespace)
-
 	if err != nil {
 		return namespaces, handleError("listNamespace", err)
 	}
 	defer rows.Close()
 
-	for rows.Next(){
+	for rows.Next() {
 		var ns database.Namespace
-		err = rows.Scan(&ns.ID,&ns.Name,ns.VersionFormat)
+
+		err = rows.Scan(&ns.ID, &ns.Name, &ns.VersionFormat)
 		if err != nil {
 			return namespaces, handleError("listNamespace.Scan()", err)
 		}
 
 		namespaces = append(namespaces, ns)
 	}
-
 	if err = rows.Err(); err != nil {
 		return namespaces, handleError("listNamespace.Rows()", err)
 	}
 
-	return  namespaces,err
+	return namespaces, err
 
 }

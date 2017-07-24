9a5b624edc6879bd6dd90c89e40444e2bbc66192
mxi4oyu <798033502@qq.com>
2017-07-24 19:21:58 +0800

    fix Severity non-string error

--- api/api.go
+++ api/api.go
@@ -67,44 +67,31 @@ func Run(cfg *Config, store database.Datastore, st *stopper.Stopper)  {
 
 	//插入一个漏洞
 
-	n1 := database.Namespace{
-		Name:          "TestInsertVulnerabilityNamespace1",
-		VersionFormat: dpkg.ParserName,
-	}
-
-	f1 := database.FeatureVersion{
-		Feature: database.Feature{
-			Name:      "TestInsertVulnerabilityFeatureVersion1",
-			Namespace: n1,
-		},
-		Version: "1.0",
-	}
-
 	v1meta := make(map[string]interface{})
-
 	v1meta["TestInsertVulnerabilityMetadata1"] = "TestInsertVulnerabilityMetadataValue1"
-	v1meta["TestInsertVulnerabilityMetadata2"] = struct {
-		Test string
-	}{
-		Test: "TestInsertVulnerabilityMetadataValue1",
-	}
 
 	v1 := database.Vulnerability{
 		Name:        "TestInsertVulnerability1",
-		Namespace:   n1,
-		FixedIn:     []database.FeatureVersion{f1},
+		Namespace:   database.Namespace{
+			Name:          "TestInsertVulnerabilityNamespace1",
+			VersionFormat: dpkg.ParserName,
+		},
+		FixedIn:     []database.FeatureVersion{database.FeatureVersion{
+			Feature: database.Feature{
+				Name:      "TestInsertVulnerabilityFeatureVersion1",
+			},
+			Version: "1.0",
+		}},
 		Severity:    database.LowSeverity,
 		Description: "TestInsertVulnerabilityDescription1",
 		Link:        "TestInsertVulnerabilityLink1",
 		Metadata:    v1meta,
 	}
 
-	err:= store.InsertVulnerabilities([]database.Vulnerability{v1}, true)
-
-	if err !=nil{
-		fmt.Println(err.Error())
+	err:= store.InsertVulnerabilities([]database.Vulnerability{v1}, false)
+	if err!=nil{
+		fmt.Println(err)
 	}
-
 
 	defer st.End()
 }
--- database/severity.go
+++ database/severity.go
@@ -29,42 +29,25 @@ var ErrFailedToParseSeverity = errors.New("failed to parse Severity from input")
 type Severity string
 
 const (
-	// UnknownSeverity is either a security problem that has not been assigned to
-	// a priority yet or a priority that our system did not recognize.
+
 	UnknownSeverity Severity = "Unknown"
 
-	// NegligibleSeverity is technically a security problem, but is only
-	// theoretical in nature, requires a very special situation, has almost no
-	// install base, or does no real damage. These tend not to get backport from
-	// upstreams, and will likely not be included in security updates unless
-	// there is an easy fix and some other issue causes an update.
+
 	NegligibleSeverity Severity = "Negligible"
 
-	// LowSeverity is a security problem, but is hard to exploit due to
-	// environment, requires a user-assisted attack, a small install base, or
-	// does very little damage.  These tend to be included in security updates
-	// only when higher priority issues require an update, or if many low
-	// priority issues have built up.
+
 	LowSeverity Severity = "Low"
 
-	// MediumSeverity is a real security problem, and is exploitable for many
-	// people.  Includes network daemon denial of service attacks, cross-site
-	// scripting, and gaining user privileges.  Updates should be made soon for
-	// this priority of issue.
+
 	MediumSeverity Severity = "Medium"
 
-	// HighSeverity is a real problem, exploitable for many people in a default
-	// installation. Includes serious remote denial of services, local root
-	// privilege escalations, or data loss.
+
 	HighSeverity Severity = "High"
 
-	// CriticalSeverity is a world-burning problem, exploitable for nearly all
-	// people in a default installation of Linux. Includes remote root privilege
-	// escalations, or massive data loss.
+
 	CriticalSeverity Severity = "Critical"
 
-	// Defcon1Severity is a Critical problem which has been manually highlighted
-	// by the team. It requires an immediate attention.
+
 	Defcon1Severity Severity = "Defcon1"
 )
 

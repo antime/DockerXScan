49eaca0987d5c7a740d85e25ac3503f7caf09bb8
mxi4oyu <798033502@qq.com>
2017-08-01 16:39:21 +0800

    add color

--- api/layerutils.go
+++ api/layerutils.go
@@ -4,8 +4,9 @@ import (
 	"github.com/MXi4oyu/DockerXScan/database"
 	"github.com/MXi4oyu/DockerXScan/api/v1"
 	"fmt"
-	"github.com/prometheus/common/log"
 	"github.com/fatih/color"
+	"github.com/kr/text"
+	"sort"
 )
 
 
@@ -31,6 +32,33 @@ type vulnerabilityInfo struct {
 	severity      database.Severity
 }
 
+type By func(v1, v2 vulnerabilityInfo) bool
+
+func (by By) Sort(vulnerabilities []vulnerabilityInfo) {
+	ps := &sorter{
+		vulnerabilities: vulnerabilities,
+		by:              by,
+	}
+	sort.Sort(ps)
+}
+
+type sorter struct {
+	vulnerabilities []vulnerabilityInfo
+	by              func(v1, v2 vulnerabilityInfo) bool
+}
+
+func (s *sorter) Len() int {
+	return len(s.vulnerabilities)
+}
+
+func (s *sorter) Swap(i, j int) {
+	s.vulnerabilities[i], s.vulnerabilities[j] = s.vulnerabilities[j], s.vulnerabilities[i]
+}
+
+func (s *sorter) Less(i, j int) bool {
+	return s.by(s.vulnerabilities[i], s.vulnerabilities[j])
+}
+
 func ShowVuls(layer v1.Layer,minSeverity database.Severity)  {
 	isSafe := true
 	hasVisibleVulnerabilities := false
@@ -52,7 +80,38 @@ func ShowVuls(layer v1.Layer,minSeverity database.Severity)  {
 		}
 	}
 
-	log.Info(vulnerabilities)
+	//log.Info(vulnerabilities)
+
+	priority := func(v1, v2 vulnerabilityInfo) bool {
+		return v1.severity.Compare(v2.severity) >= 0
+	}
+
+	By(priority).Sort(vulnerabilities)
+
+	for _, vulnerabilityInfo := range vulnerabilities {
+		vulnerability := vulnerabilityInfo.vulnerability
+		feature := vulnerabilityInfo.feature
+		severity := vulnerabilityInfo.severity
+
+		fmt.Printf("%s (%s)\n", vulnerability.Name, coloredSeverity(severity))
+
+		if vulnerability.Description != "" {
+			fmt.Printf("%s\n\n", text.Indent(text.Wrap(vulnerability.Description, 80), "\t"))
+		}
+
+		fmt.Printf("\tPackage:       %s @ %s\n", feature.Name, feature.Version)
+
+		if vulnerability.FixedBy != "" {
+			fmt.Printf("\tFixed version: %s\n", vulnerability.FixedBy)
+		}
+
+		if vulnerability.Link != "" {
+			fmt.Printf("\tLink:          %s\n", vulnerability.Link)
+		}
+
+		fmt.Printf("\tLayer:         %s\n", feature.AddedBy)
+		fmt.Println("")
+	}
 
 	if isSafe {
 		fmt.Printf("%s No vulnerabilities were detected in your image\n", color.GreenString("Success!"))
@@ -62,4 +121,19 @@ func ShowVuls(layer v1.Layer,minSeverity database.Severity)  {
 		fmt.Errorf("A total of %d vulnerabilities have been detected in your image", len(vulnerabilities))
 	}
 
+}
+
+func coloredSeverity(severity database.Severity) string {
+	red := color.New(color.FgRed).SprintFunc()
+	yellow := color.New(color.FgYellow).SprintFunc()
+	white := color.New(color.FgWhite).SprintFunc()
+
+	switch severity {
+	case database.HighSeverity, database.CriticalSeverity:
+		return red(severity)
+	case database.MediumSeverity:
+		return yellow(severity)
+	default:
+		return white(severity)
+	}
 }
